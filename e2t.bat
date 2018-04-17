db52f3aafd219374d3c36ef88608f82cb5d650e1
Anna Tsukanova <38460776+AnnabellFlem@users.noreply.github.com>
2018-04-17 23:23:22 +0300

    Delete e2t.bat

--- e2t.bat
+++
@@ -1,4 +0,0 @@
-cls
-rem start "" "C:\Program Files\nodejs\npm" install mongodb mongoose express express-session body-parser connect-mongo multer passport passport-local bcryptjs --save
-rem npm i -g npm to update
-"C:\Program Files\nodejs\node" server.js
