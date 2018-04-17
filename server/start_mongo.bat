017f958031f1d72696fa5b19214de2e77aab752c
Anna Tsukanova <38460776+AnnabellFlem@users.noreply.github.com>
2018-04-17 23:24:41 +0300

    Delete start_mongo.bat

--- server/start_mongo.bat
+++
@@ -1,2 +0,0 @@
-start "Mongo" "C:\Program Files\MongoDB\Server\3.2\bin"\mongod --storageEngine=mmapv1 --port 27017 --dbpath D:\Sites\Mongo\DB\
-rem "C:\Program Files\MongoDB\Server\3.2\bin"\mongod --dbpath D:\Sites\Mongo\DB\ --repair
